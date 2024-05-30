90ff893cf7f659c673f5632f5c59380af609f543
Godstime Pious <123419895+Godstime3-code@users.noreply.github.com>
2024-05-30 21:16:14 +1300

    Add files via upload

---
+++ application.js
@@ -0,0 +1,33 @@
+'use strict'
+let todoInput = document.querySelector('.todo-input');
+
+
+
+let todo = [];
+function addTodo(){
+    let li = document.createElement('li');
+    li.classList.add('todo-style');
+    let button = document.createElement('button');
+    button.textContent = 'Delete';
+    button.classList.add('todo-btn')
+    let todoList = todoInput.value.trim();
+    let task = document.createTextNode(todoList);
+    let ul = document.querySelector('.container ul');
+    ul.appendChild(li);
+    
+    todo.push(todoList)
+    
+
+    if(todoList){
+      li.appendChild(task);
+      li.appendChild(button);
+      todoInput.value= '';
+    }
+
+    button.addEventListener('click', ()=>{
+      li.remove();
+    })
+  
+  
+}
+
